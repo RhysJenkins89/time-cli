83ff8e425a6347ac8b36ccb0fd02e19f17622ff0
Rhys <rhys@rhysjenkins.uk>
2026-06-27 23:45:36 +0100

    Start work on fetching data

--- time.go
+++ time.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
 	if len(os.Args) > 1 {
-		router(os.Args[1])
+		router(os.Args[1]) // Ignore the first argument and accept the next X number of arguments. Ignore any afteer that.
 	} else {
 		fmt.Println("Please input a command.")
 	}
@@ -26,6 +28,13 @@ func router(command string) {
 
 func handleNow() {
 	fmt.Println("This is the now command function.")
+	// In order to get the user's location, use the client IP address
+	req, err := http.NewRequest(http.MethodGet, "http://api.open-notify.org/astros.json", nil)
+	if err != nil {
+		fmt.Println("This is the error block.")
+		log.Fatal(err)
+	}
+	fmt.Println(req)
 }
 
 func handleHelp() {
